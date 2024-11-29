a681f4bafc2f08b6d3d06b6c374ad58d8424c368
Alexaqua7 <sdh582@naver.com>
2024-11-29 19:13:07 +0900

    temp

--- lightning/train.py
+++ lightning/train.py
@@ -11,6 +11,7 @@ from utils.split import split_dataset
 import wandb
 import pandas as pd
 from utils.dataset import CollateFn
+from torchvision import transforms
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Image_Inpainting_Restoration')
@@ -37,50 +38,54 @@ def main():
     train_df = pd.read_csv(args.train_df)
     train_fold_df, valid_fold_df = split_dataset(args.seed, args.n_split, train_df)
     # Wandb initalize
-    wandb.init(
-        project=args.wandb_project,
-        entity=args.wandb_entity,
-        name=args.wandb_run_name,
-        config=vars(args)
-    )
+    # wandb.init(
+    #     project=args.wandb_project,
+    #     entity=args.wandb_entity,
+    #     name=args.wandb_run_name,
+    #     config=vars(args)
+    # )
+    transform = transforms.Compose([
+    transforms.ToTensor(),  # 이미지를 [0, 255]에서 [0, 1] 범위로 정규화하고 PyTorch 텐서로 변환
+])
 
     train_dataset = CustomImageDataset(train_fold_df, data_dir=args.train_data_dir, mode='train',min_polygon_bbox_size=args.min_polygon_bbox_size)
     valid_dataset = CustomImageDataset(valid_fold_df, data_dir=args.val_data_dir, mode='valid', min_polygon_bbox_size=args.min_polygon_bbox_size)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=CollateFn(mean=args.image_mean, std=args.image_std, mode='train'), num_workers=8, pin_memory=True, persistent_workers=True)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size*2, shuffle=False, collate_fn=CollateFn(mean=args.image_mean, std=args.image_std, mode='valid'), num_workers=8, pin_memory=True, persistent_workers=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True, persistent_workers=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size*2, shuffle=False, num_workers=8, pin_memory=True, persistent_workers=True)
+    for x,y in train_dataloader:
+        print(x.shape)
+#     model_1 = smp.UPerNet(
+#     encoder_name="resnet34",        
+#     encoder_weights="imagenet",     
+#     in_channels=1,                  
+#     classes=1,                      
+# )
 
-    model_1 = smp.UPerNet(
-    encoder_name="resnet34",        
-    encoder_weights="imagenet",     
-    in_channels=1,                  
-    classes=1,                      
-)
+#     # gray -> color
+#     model_2 = smp.UPerNet(
+#         encoder_name="resnet34",        
+#         encoder_weights="imagenet",     
+#         in_channels=1,                  
+#         classes=3,                      
+#     )
 
-    # gray -> color
-    model_2 = smp.UPerNet(
-        encoder_name="resnet34",        
-        encoder_weights="imagenet",     
-        in_channels=1,                  
-        classes=3,                      
-    )
+#     lit_ir_model = LitIRModel(model_1=model_1, model_2=model_2)
 
-    lit_ir_model = LitIRModel(model_1=model_1, model_2=model_2)
+#     checkpoint_callback = ModelCheckpoint(
+#     monitor='val_score',
+#     mode='max',
+#     dirpath='./checkpoint/',
+#     filename=f'smp-unet-resnet34'+'-{epoch:02d}-{val_score:.4f}',
+#     save_top_k=1,
+#     save_weights_only=True,
+#     verbose=True
+# )
+#     earlystopping_callback = EarlyStopping(monitor="val_score", mode="max", patience=3)
 
-    checkpoint_callback = ModelCheckpoint(
-    monitor='val_score',
-    mode='max',
-    dirpath='./checkpoint/',
-    filename=f'smp-unet-resnet34'+'-{epoch:02d}-{val_score:.4f}',
-    save_top_k=1,
-    save_weights_only=True,
-    verbose=True
-)
-    earlystopping_callback = EarlyStopping(monitor="val_score", mode="max", patience=3)
+#     trainer = L.Trainer(max_epochs=100, precision='16-mixed', callbacks=[checkpoint_callback, earlystopping_callback], detect_anomaly=False)
 
-    trainer = L.Trainer(max_epochs=100, precision='16-mixed', callbacks=[checkpoint_callback, earlystopping_callback], detect_anomaly=False)
-
-    trainer.fit(lit_ir_model, train_dataloader, valid_dataloader)
-    wandb.finish()
+#     trainer.fit(lit_ir_model, train_dataloader, valid_dataloader)
+#     wandb.finish()
 if __name__ == '__main__':
     main()
