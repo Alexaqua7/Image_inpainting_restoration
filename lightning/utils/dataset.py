ec4481a391cf129bd21bcc709e940dcab63b8814
Alexaqua7 <sdh582@naver.com>
2024-12-13 12:51:46 +0900

    FIX: 필요 없는 부분 제거거

--- lightning/utils/dataset.py
+++ lightning/utils/dataset.py
@@ -12,6 +12,89 @@ import os
 import torch
 import skimage.draw
 
+class CustomImageDataset(Dataset):
+    def __init__(self, df, data_dir='../../data/train_gt', mode='train', min_polygon_bbox_size=50):
+        self.df = df
+        self.data_dir = data_dir
+        self.mode = mode
+        self.min_polygon_bbox_size = min_polygon_bbox_size
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        # Get image path and label
+        img_path = self.df.iloc[idx, 0].split('/')[-1]  # Assuming first column is the path
+        img_path = os.path.join(self.data_dir, img_path)
+        
+        # Apply augmentation if in training mode
+        if self.mode == 'train':
+            image = Image.open(img_path)
+            image_input = get_input_image(image, self.min_polygon_bbox_size)
+            return image_input
+
+        elif self.mode == 'valid':
+            image_input = self.load_input_image(img_path)
+            return image_input
+        elif self.mode == 'test':
+            image = Image.open(img_path).convert('L')
+            return {
+                'image_gray_masked':image
+            }
+
+    def load_input_image(self, img_input_path):
+        image_input = np.load(img_input_path, allow_pickle=True)
+        return image_input.item()
+
+def get_input_image(image, min_polygon_bbox_size=50):
+    '''
+    이미지를 로드하는 함수입니다
+    Return
+    image_gt: 컬러로 복원된 ground truth
+    mask: mask가 있는 컬러 이미지
+    image_gray: 흑백의 ground truth 이미지
+    image_gray_masked: mask가 있는 흑백 이미지
+    '''
+    width, height = image.size
+    while True:
+        bbox_x1 = random.randint(0, width-min_polygon_bbox_size)
+        bbox_y1 = random.randint(0, height-min_polygon_bbox_size)
+        bbox_x2 = random.randint(bbox_x1, width)  # Ensure width > 10
+        bbox_y2 = random.randint(bbox_y1, height)  # Ensure height > 10
+        if (bbox_x2-bbox_x1)<min_polygon_bbox_size or (bbox_y2-bbox_y1)<min_polygon_bbox_size:
+            continue
+        
+        mask_bbox = [bbox_x1, bbox_y1, bbox_x2, bbox_y2]
+        mask_width = bbox_x2-bbox_x1
+        mask_height = bbox_y2-bbox_y1
+    
+        num_points = random.randint(3,20)
+        polygon_func = random.choice([
+            random_polygon,
+            random_star_shaped_polygon,
+            random_convex_polygon
+        ])
+        polygon = polygon_func(num_points=num_points) #scaled 0~1
+        polygon = [(round(r*mask_width), round(c*mask_height)) for r,c in polygon]
+        polygon_mask = skimage.draw.polygon2mask((mask_width, mask_height), polygon)
+        if np.sum(polygon_mask)>(min_polygon_bbox_size//2)**2:
+            break
+    full_image_mask = np.zeros((width, height), dtype=np.uint8)
+    full_image_mask[bbox_x1:bbox_x2, bbox_y1:bbox_y2] = polygon_mask
+    
+    image_gray = image.convert('L')
+    image_gray_array = np.array(image_gray)  # Convert to numpy array for manipulation
+    random_color = random.randint(0, 100)  # Random grayscale color
+    image_gray_array[full_image_mask == 1] = random_color
+    image_gray_masked = Image.fromarray(image_gray_array)
+
+    return {
+        'image_gt':image,
+        'mask':full_image_mask,
+        'image_gray':image_gray,
+        'image_gray_masked':image_gray_masked
+    }
+
 class StratifiedImageDataset(Dataset):
     def __init__(self, df, data_dir='../../data/train_gt', mode='train', min_polygon_bbox_size=50, max_polygon_bbox_size=100, pct=0.1, max_points=20, transforms=None):
         self.df = df
